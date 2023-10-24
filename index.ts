6ef2b10a258b827cc646db10db888335d47cd9fe
PiotrekZ <zajkowski.piotr@interia.pl>
2023-10-24 22:05:18 +0200

    Init commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import express, {json} from "express";
+import cors from "cors";
+import "express-async-errors";
+import {handleError} from "./utils/errors";
+import rateLimit from "express-rate-limit";
+import {roadRouter} from "./routers/road.router";
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(json());
+app.use(rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 100,
+}));
+
+app.use('/road', roadRouter);
+app.use(handleError);
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on port http://localhost:3001')
+})
