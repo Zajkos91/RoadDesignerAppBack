85d2cffa19ba603d1d3987ddac1e08bd813887cd
PiotrekZ <zajkowski.piotr@interia.pl>
2023-10-25 19:33:03 +0200

    Created a roadRouter with get, post, patch, delete paths.

---
+++ routers/road.router.ts
@@ -0,0 +1,42 @@
+import {Router} from "express";
+import {RoadRecord} from "../records/road.record";
+import {ValidationError} from "../utils/errors";
+
+export const roadRouter = Router()
+    .get('/search/:name?', async (req, res) => {
+
+        const roads = await RoadRecord.findAll(req.params.name ?? '');
+
+        res.json(roads);
+    })
+
+    .get('/:id', async(req, res) => {
+        const road = await RoadRecord.getOne(req.params.id);
+        res.json(road);
+    })
+
+    .post('/', async (req, res) => {
+        const road  = new RoadRecord(req.body);
+        await road.insert();
+        res.json(road);
+    })
+
+    .delete('/:id', async (req, res) => {
+        const road  = await RoadRecord.getOne(req.params.id);
+
+        if(road === null) {
+            throw new ValidationError('Not found road with that Id');
+        }
+        await road.delete();
+        res.json(road);
+    })
+
+    .patch('/:id', async (req, res) => {
+        const road  = await RoadRecord.getOne(req.params.id);
+
+        if(road === null) {
+            throw new ValidationError('Not found road with that Id');
+        }
+        await road.update();
+        res.json(road);
+    });
